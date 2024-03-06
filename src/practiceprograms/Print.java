fa545adbe6502bd1714b72318fcc03ef33411543
Rohitkamble5541 <rohit.kamble.world@gmail.com>
2024-03-06 22:45:17 +0530

    Initial commit

---
+++ src/practiceprograms/Print.java
@@ -0,0 +1,12 @@
+package practiceprograms;
+
+public class Print {
+
+
+    // printing statment continues
+    public static void main(String[] args) {
+        System.out.print("Hello Word");
+        System.out.print("Rohit Kamble");
+        System.out.print("LMS India Solutions Pvt Ltd");
+    }
+}
